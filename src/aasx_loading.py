4b46a83098136452eb749624781d921b3b1ac191
unknown <ch.kwak@interxlab.com>
2025-02-05 12:59:17 +0900

    file update

--- src/aasx_loading.py
+++ src/aasx_loading.py
@@ -1,5 +1,7 @@
 import zipfile
+from collections import defaultdict
 import xml.etree.ElementTree as ET
+
 
 # AASX 파일 Loading
 def read_aasx_file(aasx_path):
@@ -8,7 +10,7 @@ def read_aasx_file(aasx_path):
         aasx = zipfile.ZipFile(aasx_path, 'r')
         # ZIP 내부의 파일 목록 출력 (AASX 내부 구조 확인)
         file_list = aasx.namelist()
-        print(" - AASX 파일 내의 파일 목록:")
+        print(" - AASX 파일 내의 파일 목록:", file_list)
 
         for file_name in file_list:
             if "aasx/" in file_name and ".aas.xml" in file_name and "/_rels/" not in file_name:
@@ -18,12 +20,33 @@ def read_aasx_file(aasx_path):
                 break
         
         with aasx.open(target_xml_file) as xml_file:
-                tree = ET.parse(xml_file)
-                root = tree.getroot()
+            tree = ET.parse(xml_file)
 
-                return root, target_file_name
+            return tree, target_file_name
 
     except Exception as e:
         print(f"AASX 파일 Loading 중 오류 발생: {e}")
         return None
     
+
+def extract_xml_data(root):
+    
+    # root = xml_data.getroot()
+    data_collection = defaultdict(list)
+
+    print(root)
+    for aas in root.findall(".//AssetAdministrationShell"):
+        aas_id = aas.find("id").text if aas.find("id") is not None else "Unknown_AAS"
+        data_collection["AAS"].append({"id": aas_id, "xml": aas})
+
+    # Submodels 추출
+    for submodel in root.findall(".//Submodel"):
+        submodel_id = submodel.find("id").text if submodel.find("id") is not None else "Unknown_Submodel"
+        data_collection["Submodels"].append({"id": submodel_id, "xml": submodel})
+
+    # ConceptDescriptions 추출
+    for concept in root.findall(".//ConceptDescription"):
+        concept_id = concept.find("id").text if concept.find("id") is not None else "Unknown_ConceptDescription"
+        data_collection["ConceptDescriptions"].append({"id": concept_id, "xml": concept})
+
+    return data_collection
--- src/main.py
+++ src/main.py
@@ -1,16 +1,22 @@
 import os
 
 from db import find_data
-from aasx_loading import read_aasx_file
+from aasx_loading import read_aasx_file, extract_xml_data
 
 
 def main():
-    file_name = 'Injection_Molding_Machine.aasx'
+    file_name = 'DemoKit.aasx'
     aasx_file_path = os.path.join(os.getcwd(), 'aasx_file', file_name)
-    xml_file, DB_name = read_aasx_file(aasx_file_path)
+    xml_data,DB_name = read_aasx_file(aasx_file_path)
 
+    print("xml_data: ",xml_data)
+    print("DB_name: ",DB_name) 
+
+    xml_data_collection = extract_xml_data(xml_data)
+    print("xml_data_collection: ",xml_data_collection)
+    
 
 if __name__ == "__main__":
     main()
 
-print(find_data('aas_repo'))
+#print(find_data('aas_repo'))
